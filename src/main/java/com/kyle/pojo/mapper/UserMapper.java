b8c48ab2a062c201060bca0e5f8d658addb9943b
Kyle Xiong <kyle.xiong@software.dell.com>
2016-08-10 17:03:52 +0800

    add insert , update , delete to master

--- src/main/java/com/kyle/pojo/mapper/UserMapper.java
+++ src/main/java/com/kyle/pojo/mapper/UserMapper.java
@@ -5,4 +5,10 @@ import com.kyle.pojo.User;
 public interface UserMapper {
 
 	public User getUser(int id);
+	
+	public void insertUser(User user);
+	
+	public void updateUser(User user);
+	
+	public void deleteUser(User user);
 }
--- src/test/java/com/kyle/test/TestMyBatis.java
+++ src/test/java/com/kyle/test/TestMyBatis.java
@@ -15,23 +15,100 @@ import com.kyle.pojo.User;
 import com.kyle.pojo.mapper.UserMapper;
 
 public class TestMyBatis extends TestCase {
-
-	@Test
-	public void testMyBatis() {
-		String source = "mybatis-config.xml";
-		SqlSession session = null;
+    
+	private SqlSessionFactory sqlSessionFactory = null;
+	
+	@Override
+	protected void setUp() throws Exception {
 		try {
+			String source = "mybatis-config.xml";
 			InputStream inputStream = Resources.getResourceAsStream(source);
-			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder()
+			sqlSessionFactory = new SqlSessionFactoryBuilder()
 					.build(inputStream);
-			session = sqlSessionFactory.openSession();
 			
-			UserMapper mapper = session.getMapper(UserMapper.class);
-			User user = mapper.getUser(2);
-			System.out.println(user);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	@Test
+	public void testSelect() {
+		SqlSession session = null;
+		
+		try {
+			session = sqlSessionFactory.openSession();
+			UserMapper mapper = session.getMapper(UserMapper.class);
+			User user = mapper.getUser(2);
+			System.out.println(user);
+		} 
+		finally
+		{
+			if(null != session)
+			{
+				session.close();
+			}
+		}
+	}
+	
+    @Test
+	public void testInsert() {
+		SqlSession session = null;
+		
+		try {
+			session = sqlSessionFactory.openSession();
+			UserMapper mapper = session.getMapper(UserMapper.class);
+			User user = new User();
+			user.setName("许三多");
+			user.setAge(26);
+			mapper.insertUser(user);
+			session.commit();
+			System.out.println("insert successfully!");
+		} 
+		finally
+		{
+			if(null != session)
+			{
+				session.close();
+			}
+		}
+	}
+	
+	@Test
+	public void testUpdate() {
+		SqlSession session = null;
+		
+		try {
+			session = sqlSessionFactory.openSession();
+			UserMapper mapper = session.getMapper(UserMapper.class);
+			User user = new User();
+			user.setAge(20);
+			user.setId(5);
+			mapper.updateUser(user);
+			session.commit();
+			System.out.println("update successfully!");
+		} 
+		finally
+		{
+			if(null != session)
+			{
+				session.close();
+			}
+		}
+	}
+	
+	@Test
+	public void testDelete() {
+		SqlSession session = null;
+		
+		try {
+			session = sqlSessionFactory.openSession();
+			UserMapper mapper = session.getMapper(UserMapper.class);
+			User user = new User();
+			user.setId(5);
+			mapper.deleteUser(user);
+			session.commit();
+			System.out.println("delete successfully!");
+		} 
 		finally
 		{
 			if(null != session)
